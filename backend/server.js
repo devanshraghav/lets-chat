74fdec3dc2c67b1906b52bf9e0186000b9e83d33
unknown <devanshraghav2000@gmail.com>
2024-06-25 00:02:23 +0530

    Connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send('API is running');
+  res.send("API is running");
 });
 
-app.get("/api/chat",(req,res)=>{
-    res.send(chats);
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
 app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((chat) => chat._id == req.params.id);
@@ -20,4 +23,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
+app.listen(PORT, console.log(`Server is up and running on port ${PORT}`.yellow.bold));
